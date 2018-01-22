4d036a0f259d8433a086b90115959672ee827c94
Graham <gk@Grahams-iMac.local>
2018-01-22 07:43:42 +0000

    notify listeners when config loaded

--- main/config-service.js
+++ main/config-service.js
@@ -2,16 +2,23 @@ class ConfigService {
   constructor() {
     this.loaded = false;
     this.config = {};
+    this.loadListeners = [];
   }
   setConfig(config) {
     this.loaded = true;
     this.config = config;
+    for (let l of this.loadListeners) {
+      l();
+    }
   }
   getConfig() {
     return this.config;
   }
   isLoaded() {
     return this.loaded;
+  }
+  addLoadListener(l) {
+    this.loadListeners.push(l);
   }
 }
 
